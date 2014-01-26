f1219eb1c3a9bd28be9e118b349ad2522da4d3b7
Ayoosh Bansal <ayoosh4@gmail.com>
2014-01-26 15:10:57 -0600

    Transmitter module edits

--- transmitter.v
+++ transmitter.v
@@ -17,12 +17,12 @@ always@(posedge clk) begin
 	if(reset)begin
 		t_shift_reg <= 0x3ff;
 		t_count <= 0;
-		t_count <= 0;
+		t_busy <= 0;
 	end
-	else if(baud_enable && t_busy) begin
+	else if(baud_enable && t_busy) begin // xxx does not needs t_busy
 		for(i=0 ; i < 4'b1000; i=i+1)
-			t_shift_reg[i] <= t_shift_reg[i] + 1;
-		t_shift_reg[7] <= 1'b1;
+			t_shift_reg[i] <= t_shift_reg[i+1];
+		t_shift_reg[9] <= 1'b1;
 		t_count <= t_count - 1'b1;
 		if(t_count == 0)
 			t_busy <= 1'b0;
