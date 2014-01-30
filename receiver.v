ae6e17d3016cc24da039f1d3bcfb9efaa0413e08
Ayoosh Bansal <ayoosh4@gmail.com>
2014-01-29 23:13:52 -0600

    receiver work in progress

--- receiver.v
+++ receiver.v
@@ -5,9 +5,13 @@ module receiver(input clk, input rst, input r_enable, input rxd, input rec_enabl
 	reg [3:0]i;
 	reg [3:0]zcnt;
 	reg start; // have we detected the start bit
-	reg [2:0]bitcnt;	
+	reg misery;
+	reg [2:0]bitcnt;
+	reg receive_sig;
 
 	always@(posedge clk) begin
+		receive_sig <= rxd;
+
 		if (rst) begin
 			data <= 0;
 			rda <= 0;
@@ -15,29 +19,37 @@ module receiver(input clk, input rst, input r_enable, input rxd, input rec_enabl
 			zcnt <= 0;
 			start <= 0;
 			bitcnt <= 0;
+			misery <= 0;
 		end
 
 		else if (r_enable) begin
-			if (start == 0 && rxd == 1) begin // Sanity check only to bypass idle condition
+			if (start == 0 && receive_sig == 1) begin // Sanity check only to bypass idle condition
 			end
-			else if (rxd == 0 && start == 0) begin
-				zcnt <= zcnt + 1;
-				if (i == 0) begin
-					if (zcnt > 4'd8) begin
-						start <= 1;
-					end
-					i <= 4'b1111;
-					zcnt <= 0;
+			else if (start == 0) begin // First worry about start bit
+				if (receive_sig == 0 && misery == 0) begin
+					misery <= 1;
+					zcnt <= zcnt + 1;
+					i <= i - 1;
 				end
-				else begin
+				else if (misery == 1) begin
+					if (i != 0) begin
 						i <= i - 1;
+						if (receive_sig == 0) begin
+							zcnt <= zcnt + 1;
+						end		
+					end
+					else begin
+						if (zcnt > 4'd8) begin
+							start <= 1;
+						end
+						i <= 4'b1111;
+						zcnt <= 0;
+						misery <= 0;
+					end
 				end
 			end
 
 			else if (start == 1) begin
-				if (rxd == 0) begin
-					zcnt <= zcnt + 1;
-				end
 				if (i == 0) begin
 					if (zcnt > 4'd8) begin
 						data[bitcnt] <= 0;
@@ -51,12 +63,15 @@ module receiver(input clk, input rst, input r_enable, input rxd, input rec_enabl
 				end
 				else begin
 					i <= i - 1;
+					if (receive_sig == 0) begin
+						zcnt <= zcnt + 1;
+					end
 				end
 				
-				if (bitcnt == 3'b111) begin
-
-
-
+				if (bitcnt == 3'b111) begin	
+			
+			
+			
 			end			
 		end
 	end
