97e84c64249e85a44e594e0fe130aac60e5cb4c4
smurthy123 <smurthy@wisc.edu>
2014-02-02 13:32:34 -0600

    Commented receiver_test

--- receive_test.v
+++ receive_test.v
@@ -1,3 +1,8 @@
+//Toplevel of the test bench which has interacting modules.
+//Here, 'terminal is a module which mimics the behaviour of the computer which sends the data to the FPGA through the txd and rxd lines.
+//Spart module receives data from the rxd line and sends it to the driver on the databus. It also sends the data in serial form at the specifies baud rate to through the serial port.
+//Driver is the processor which monitors the TBR and RDA lines and sends or receives data respectively.
+
 `timescale 1ns/1ps
 module t_receiver;
 
@@ -21,8 +26,8 @@ end
 always
 #1 clk = ~clk;
 
-terminal term(.clk(clk), .rst(rst1), .data(data));
-spart spt_module(.clk(clk), .rst(rst), .iocs(iocs), .iorw(iorw),.rda(rda), .tbr(tbr), .ioaddr(io), .databus(data_bus), .txd(tr), .rxd(data));
-driver processor(.clk(clk), .rst(rst), .br_cfg(baud), .iocs(iocs),.iorw(iorw), .rda(rda), .tbr(tbr), .ioaddr(io), .databus(data_bus));
+terminal term(.clk(clk), .rst(rst1), .data(data));    //Instantiation of the terminal which emulates the behaviour of a computer
+spart spt_module(.clk(clk), .rst(rst), .iocs(iocs), .iorw(iorw),.rda(rda), .tbr(tbr), .ioaddr(io), .databus(data_bus), .txd(tr), .rxd(data)); //Instantiates the spart module
+driver processor(.clk(clk), .rst(rst), .br_cfg(baud), .iocs(iocs),.iorw(iorw), .rda(rda), .tbr(tbr), .ioaddr(io), .databus(data_bus)); //Instantiation of the driver
 
 endmodule  
