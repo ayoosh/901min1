f9e3f2bd7420bd7a8eb48b3fd4138b76a8696404
smurthy123 <smurthy@wisc.edu>
2014-02-02 13:53:15 -0600

    Commented files

--- terminal.v
+++ terminal.v
@@ -8,15 +8,15 @@ assign data = data_send[0];
 
 always@(posedge clk)begin
 	if(rst)begin
-		data_send <= 12'hAAB; // 1 0101 0101 0 11
-		baud_counter <= 16'h28A7;
+		data_send <= 12'hAAB; // 1-01010101-0-11 -> Stop bit - 8 data bits MSB to LSB - Start bit - stop bits...
+		baud_counter <= 16'h28A7; // generates a baud rate of 9600. Changing its values changes the baud rates to 4800, 19200, 38400
 	end
 	else begin
 		if(baud_counter == 0)begin
 			baud_counter <= 16'h28A7;
 			for(i=0 ;i < 4'b1011; i = i+1)
 				data_send[i] <= data_send[i+1];
-			data_send[11] <=1'b1 ;
+			data_send[11] <= data_send[0];
 		end
 		else
 			baud_counter <= baud_counter - 1;
--- test_down_cnt_16.v
+++ test_down_cnt_16.v
@@ -1,8 +1,13 @@
+//Testbench to check the functionality of the down-counter in the baud rate generator. 
+//This testbench gives a pseudo value to the divisor value and starts the counter through the wr_en signal. 
+//r_enable signal runs at 16X of the baud rate and acts as enable signal for the receiver block. t_enable runs at the baud rate and acts as an enable for the transmitter block.
+
 `timescale 1ns/1ps
 module t_down_cnt_16;
+
 reg rst, clk, wr_en;
 reg [15:0]in;
-wire zero;
+wire r_enable, t_enable;
 
 downcounter_16 dcnt_16(rst, clk, wr_en, in, r_enable, t_enable);
 
@@ -10,8 +15,8 @@ initial begin
 	rst = 1;
 	clk = 1;
 	#10 rst = 0;
-	#10 in = 16'b0000000000001111;
-	#2 wr_en = 1;
+	#10 in = 16'b0000000000001111; //starting value for the counter
+	#2 wr_en = 1; // Starts the down-count operation
 	#2 wr_en = 0;
 end
 
